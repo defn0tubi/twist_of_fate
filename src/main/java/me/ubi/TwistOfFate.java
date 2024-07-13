9584f4b36d8be10b34eebb3dc248776425ea5b48
Definitely <whitechyt@gmail.com>
2024-07-13 20:46:34 +0300

    Change guild id

--- src/main/java/me/ubi/TwistOfFate.java
+++ src/main/java/me/ubi/TwistOfFate.java
@@ -7,6 +7,8 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.Objects;
+
 public class TwistOfFate {
 
     public static void main(String[] args) throws InterruptedException {
@@ -17,7 +19,7 @@ public class TwistOfFate {
                 .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_MEMBERS)
                 .build();
 
-        jda.awaitReady().addEventListener(new SlashCommandHandler(jda, jda.getGuildById("721880018673860608")));
+        jda.awaitReady().addEventListener(new SlashCommandHandler(jda, Objects.requireNonNull(jda.getGuildById("553158091223334914"))));
 
     }
 }
