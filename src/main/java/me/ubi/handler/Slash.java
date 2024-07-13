078c2170df3563d0818d7886fd3b6a84c6f0012f
Definitely <whitechyt@gmail.com>
2024-07-13 20:45:07 +0300

    Add command options initialization

--- src/main/java/me/ubi/handler/Slash.java
+++ src/main/java/me/ubi/handler/Slash.java
@@ -2,8 +2,11 @@ package me.ubi.handler;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -16,7 +19,17 @@ public interface Slash {
 
     boolean isSpecificGuild();
 
-    default CommandData getCommandData() {
-        return new CommandDataImpl(getName(), getDescription());
+    default List<OptionData> getOptions() {
+        return List.of();
     }
+
+    default CommandData getCommandData() {
+        CommandDataImpl commandData = new CommandDataImpl(getName(), getDescription());
+
+        for (OptionData option : getOptions()) {
+            commandData.addOptions(option);
+        }
+        return commandData;
+    }
+
 }
