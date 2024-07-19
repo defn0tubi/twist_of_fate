5dd65b7c93e24a9e5bb0a249492a38ec92fb2b74
Definitely <whitechyt@gmail.com>
2024-07-19 23:14:42 +0300

    Unnecessary complex implementation of double dice

--- src/main/java/me/ubi/util/Dice.java
+++ src/main/java/me/ubi/util/Dice.java
@@ -1,16 +1,37 @@
 package me.ubi.util;
 
+import me.ubi.game.Event;
+import me.ubi.game.Game;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Dice {
     private int sides;
+    private Game game;
 
-    public Dice(int sides) {
+    private static final Random rng = new Random();
+
+    public Dice(int sides, Game game) {
         this.sides = sides;
+        this.game = game;
+    }
+
+    public static ArrayList<Integer> roll(int sides, Game game) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(rng.nextInt(1, sides + 1));
+
+        if (game.getCurrentEvent() == Event.DOUBLE_DICE)
+            result.add(rng.nextInt(1, sides + 1));
+
+        return result;
     }
 
     public static int roll(int sides) {
-        Random rng = new Random();
         return rng.nextInt(1, sides + 1);
+    }
+
+    public static String getRollString(ArrayList<Integer> rolls) {
+        return rolls.get(0).toString() + " -> " + rolls.get(1).toString();
     }
 }
